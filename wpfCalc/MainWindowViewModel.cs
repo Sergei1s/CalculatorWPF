2028f30ae35ad0de649508ca7371068db660447e
Dmitry Kazankov <KazankovD@ryanair.com>
2018-08-06 14:04:55 +0200

    Выделил контрол калькулятора в отдельную вью, добавил вью модели, простой биндинг на тайтл

---
+++ wpfCalc/MainWindowViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using wpfCalc.Annotations;
+
+namespace wpfCalc
+{
+    internal sealed class MainWindowViewModel
+    {
+        private string title;
+
+        public string Title
+        {
+            get => title;
+            set
+            {
+                title = value;
+                OnPropertyChanged(nameof(Title));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ wpfCalc/Views/CalculatorView.xaml.cs
@@ -0,0 +1,13 @@
+﻿using wpfCalc.ViewModels;
+
+namespace wpfCalc.Views
+{
+    public partial class CalculatorView
+    {
+        public CalculatorView()
+        {
+            InitializeComponent();
+            DataContext = new CalculatorViewModel();
+        }
+    }
+}
